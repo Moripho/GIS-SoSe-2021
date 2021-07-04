0589c91651be2dafb4201a4548ad1585bea0e635
Moripho <jonas.kopp@hs-furtwangen.de>
2021-07-04 13:56:06 +0200

    Überarbeitung der HTML-Seiten und der Skripte inkl. Servereinbindung 2

---
+++ client/application.ts
@@ -0,0 +1,6 @@
+const toggleButton: HTMLElement = <HTMLElement>document.getElementsByClassName("toggleButton")[0];
+const navBarLinks: HTMLElement = <HTMLElement>document.getElementsByClassName("navBarLinks")[0];
+
+toggleButton.addEventListener("click", () => {
+    navBarLinks.classList.toggle("active");
+});
