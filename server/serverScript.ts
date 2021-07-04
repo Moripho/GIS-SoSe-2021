17770f9081e64790e1120e44b91717d079065216
Moripho <jonas.kopp@hs-furtwangen.de>
2021-07-04 21:07:48 +0200

    Überarbeitung der HTML-Seiten und der Skripte inkl. Servereinbindung 6

--- server/serverScript.ts
+++ server/serverScript.ts
@@ -1,25 +1,21 @@
-import * as Http from "http";
-import * as Url from "url";
+import * as Http from "http";               // HTTP-Modul, damit Server über HTTP Protokoll erreicht werden kann
+import * as Url from "url";                 // URL-Modul, um query-String extrahieren und interpretieren zu können
 
-export namespace serverModulprüfung {
-    interface User {
-        uname: string;
+export namespace serverModulprüfung {       // export vor Namecpace, aufgrund Typescript Interpretation und um Erzeugung einer fehlerhaften js-Datei zu verhinden
+
+    interface User {            // Interface für User mit Nutzernamen, Passwort und Favoritenliste
+        username: string;
         password: string;
     }
 
-    interface ServerMeldung {
+    interface ServerMeldung {   // Interface für Server Meldung
         error: string;          // Error Message, wenn Username vorhanden
         message: string;        // Nachricht, wenn User erfolgreich angelegt wurde
     }
 
-    interface Recipe {
-        username: string;
-        title: string;
-        ingredients: string[];
-        preparation: string;
-    }
+    let userData: Mongo.Collection;         // MongoDB-Datenbank mit allen Nutzerdaten
+    let recipeData: Mongo.Collection;       // MongoDB-Datenbank mit allen Rezeptdaten
 
-    let recipeData: Mongo.Collection;
 
     let port: number = Number(process.env.PORT);    // Erstellen der Port-Adresse, Process-Objekt liefert Port. Kann aber auch string oder undefined sein, daher auf number casten
     if (!port) port = 8100;                         // Falls port keinen Wert hat, wird ihm 8100 zugewiesen.
